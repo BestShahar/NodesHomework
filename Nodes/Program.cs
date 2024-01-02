496513bbb4411ea5888b50ce559b4b26748e748d
User <User@LAB3-3>
2024-01-02 09:35:45 +0200

    fun

--- Nodes/Program.cs
+++ Nodes/Program.cs
@@ -47,129 +47,161 @@ public class Node<T>
     }
 }
 
-public class Program<T>
+
+public class Program
 {
-    public static void AddToStart(Node<T>node, Node<T>lst)
+    public static Node<int> Function1(int from, int to, int quantity)
     {
-        node.SetNext(lst);
-    }
-    public static void AddToEnd(Node<T> node, Node<T> lst)
-    {
-       while (lst.HasNext() == true)
-        {
-            lst = lst.GetNext();
-            AddToEnd(node, lst);
-        }
-        
-    }
-    
-    public static void Add2Middle(Node<int>node, Node<int>lst)
-    { 
-        while (lst.HasNext() == true&&lst.GetNext().GetValue()<node.GetValue())
-        {
-            lst = lst.GetNext(); 
-        }
-        node.SetNext(lst.GetNext());
-        lst.SetNext(node);
-   
-    }
-    //פעולה המקבלת מצביע לחוליה 
-    //מחזירה אמת אם החוליות מסודרות בסדר עולה
-    //ושקר אחרת
-    //אורך הקלט n:
-    //המקרה הגרוע:
-    //הפעולה מתבצעת.... ובכל סיבוב הלולאה מתבצעות.... מכאן שסיבוכיות הפעולה: O(n)
-    public static bool IsAscending(Node<int> lst)
-    {
-        Node<int> p = lst;
-        if (p == null) return true;    //אם השרשרת ריקה אז זה לא יורד לכן עולה
+        Random rnd = new Random();
+        Node<int> head = new Node<int>(rnd.Next(from, to + 1));
+        #region Add To Tail
+        //Node<int> tail = head;
+        //for (int i = 2; i <= quantity; i++)
+        //{
+        //    Node<int> toInsert = new Node<int>(rnd.Next(from, to + 1));
+        //    tail.SetNext(toInsert);
+        //    tail = tail.GetNext();
+        //}
+        #endregion
 
-        while (p != null && p.GetNext() != null)
+        #region Add To Head
+        for (int i = 2; i <= quantity; i++)
         {
-            if (p.GetValue() > p.GetNext().GetValue()) return false;
-            p = p.GetNext();
+            head = new Node<int>(rnd.Next(from, to + 1), head);
         }
-        return true;
+        #endregion
+        return head;
+    }
+    public static Node<T> Deletevalue<T>(Node<T> lst, T value) 
+     {
+        Node<T> head = lst;
+        if(lst.GetValue().Equals(value))
+        {
+            hed
+        }
+        return head;
+     }
+public static void AddToStart<T>(Node<T> node, Node<T> lst)
+{
+    node.SetNext(lst);
+}
+public static void AddToEnd<T>(Node<T> node, Node<T> lst)
+{
+    while (lst.HasNext() == true)
+    {
+        lst = lst.GetNext();
+        AddToEnd(node, lst);
     }
 
-    //אורך הקלט n:
-    //המקרה הגרוע:
-    //הפעולה מבצעת ....  קריאות ובכל קריאה הלולאה מתבצעות.... מכאן שסיבוכיות הפעולה: O(n)
+}
 
-    //כמו קודם רק מימוש רקורסיבי
-    public static bool IsAscendingRecursive(Node<int> p)
+public static void Add2Middle(Node<int> node, Node<int> lst)
+{
+    while (lst.HasNext() == true && lst.GetNext().GetValue() < node.GetValue())
     {
-        if (p == null) return true;
-        if (p.GetNext() == null) return true;
+        lst = lst.GetNext();
+    }
+    node.SetNext(lst.GetNext());
+    lst.SetNext(node);
+
+}
+//פעולה המקבלת מצביע לחוליה 
+//מחזירה אמת אם החוליות מסודרות בסדר עולה
+//ושקר אחרת
+//אורך הקלט n:
+//המקרה הגרוע:
+//הפעולה מתבצעת.... ובכל סיבוב הלולאה מתבצעות.... מכאן שסיבוכיות הפעולה: O(n)
+public static bool IsAscending(Node<int> lst)
+{
+    Node<int> p = lst;
+    if (p == null) return true;    //אם השרשרת ריקה אז זה לא יורד לכן עולה
+
+    while (p != null && p.GetNext() != null)
+    {
         if (p.GetValue() > p.GetNext().GetValue()) return false;
-        return IsAscendingRecursive(p.GetNext());
+        p = p.GetNext();
     }
+    return true;
+}
 
-    //פעולה גנרית המחזירה אמת אם 
-    //x 
-    //קיים בשרשרת החוליות 
-    //lst
-    //ושקר אחחרת
-    //שימו לב שבפעולה גנרית אין 
-    //לא ניתן להשתמש ב
-    //==
-    //יש להתשמש בפעולה של
-    //object
-    //Equals
+//אורך הקלט n:
+//המקרה הגרוע:
+//הפעולה מבצעת ....  קריאות ובכל קריאה הלולאה מתבצעות.... מכאן שסיבוכיות הפעולה: O(n)
 
-    //אורך הקלט n:
-    //המקרה הגרוע:
-    //הפעולה מתבצעת.... ובכל סיבוב הלולאה מתבצעות.... מכאן שסיבוכיות הפעולה: O(n)
+//כמו קודם רק מימוש רקורסיבי
+public static bool IsAscendingRecursive(Node<int> p)
+{
+    if (p == null) return true;
+    if (p.GetNext() == null) return true;
+    if (p.GetValue() > p.GetNext().GetValue()) return false;
+    return IsAscendingRecursive(p.GetNext());
+}
 
-    public static bool IsExists<T>(Node<T> lst, T x)
+//פעולה גנרית המחזירה אמת אם 
+//x 
+//קיים בשרשרת החוליות 
+//lst
+//ושקר אחחרת
+//שימו לב שבפעולה גנרית אין 
+//לא ניתן להשתמש ב
+//==
+//יש להתשמש בפעולה של
+//object
+//Equals
+
+//אורך הקלט n:
+//המקרה הגרוע:
+//הפעולה מתבצעת.... ובכל סיבוב הלולאה מתבצעות.... מכאן שסיבוכיות הפעולה: O(n)
+
+public static bool IsExists<T>(Node<T> lst, T x)
+{
+    Node<T> p = lst;
+
+    while (p != null)
     {
-        Node<T> p = lst;
-
-        while (p != null)
-        {
-            if (p.GetValue().Equals(x)) return true;
-            p = p.GetNext();
-        }
-        return false;
+        if (p.GetValue().Equals(x)) return true;
+        p = p.GetNext();
     }
-    //אורך הקלט n:
-    //המקרה הגרוע:
-    //הפעולה מבצעת ....  קריאות ובכל קריאה הלולאה מתבצעות.... מכאן שסיבוכיות הפעולה: O(n)
+    return false;
+}
+//אורך הקלט n:
+//המקרה הגרוע:
+//הפעולה מבצעת ....  קריאות ובכל קריאה הלולאה מתבצעות.... מכאן שסיבוכיות הפעולה: O(n)
 
 
-    public static bool IsExistsRecursive<T>(Node<T> lst, T x)
-    {
-        if (lst == null) return false;
-        if (lst.GetValue().Equals(x)) return true;
-        return IsExistsRecursive(lst.GetNext(), x);
-    }
+public static bool IsExistsRecursive<T>(Node<T> lst, T x)
+{
+    if (lst == null) return false;
+    if (lst.GetValue().Equals(x)) return true;
+    return IsExistsRecursive(lst.GetNext(), x);
+}
 
 
 
-    public static void Main()
-    {
-        Node<int> lst1 = new Node<int>(4, new Node<int>(5, new Node<int>(6, new Node<int>(7))));//[4, next]=>[5, next]=>[6, next]=>[7, next]=>null
+public static void Main()
+{
+    Node<int> lst1 = new Node<int>(4, new Node<int>(5, new Node<int>(6, new Node<int>(7))));//[4, next]=>[5, next]=>[6, next]=>[7, next]=>null
 
-        Console.WriteLine(IsAscending(lst1));//should print True
-        Console.WriteLine(IsAscendingRecursive(lst1));//should print True
+    Console.WriteLine(IsAscending(lst1));//should print True
+    Console.WriteLine(IsAscendingRecursive(lst1));//should print True
 
-        Node<int> lst2 = new Node<int>(4, new Node<int>(5, new Node<int>(6, new Node<int>(2))));//[4, next]=>[5, next]=>[6, next]=>[2, next]=>null
-        Console.WriteLine(IsAscending(lst2));//should print False
-        Console.WriteLine(IsAscendingRecursive(lst2));//should print False
-        Node<int> lst3 = new Node<int>(4, new Node<int>(5, new Node<int>(4, new Node<int>(9))));//[4, next]=>[5, next]=>[4, next]=>[9, next]=>null
-        Console.WriteLine(IsAscending(lst3));//should print False
-        Console.WriteLine(IsAscendingRecursive(lst3));//should print False
+    Node<int> lst2 = new Node<int>(4, new Node<int>(5, new Node<int>(6, new Node<int>(2))));//[4, next]=>[5, next]=>[6, next]=>[2, next]=>null
+    Console.WriteLine(IsAscending(lst2));//should print False
+    Console.WriteLine(IsAscendingRecursive(lst2));//should print False
+    Node<int> lst3 = new Node<int>(4, new Node<int>(5, new Node<int>(4, new Node<int>(9))));//[4, next]=>[5, next]=>[4, next]=>[9, next]=>null
+    Console.WriteLine(IsAscending(lst3));//should print False
+    Console.WriteLine(IsAscendingRecursive(lst3));//should print False
 
-        Node<char> lst4 = new Node<char>('t', new Node<char>('A', new Node<char>('l', new Node<char>('s', new Node<char>('i')))));//['t', next]=>['a', next]=>['l', next]=>['s', next]=>['i', next]=>null
-        Console.WriteLine(IsExists(lst1, 5));//should print True
-        Console.WriteLine(IsExists(lst4, 'i'));//should print True
-        Console.WriteLine(IsExists(lst4, 'I'));//should print False
-        Console.WriteLine(IsExistsRecursive(lst1, 5));//should print True
-        Console.WriteLine(IsExistsRecursive(lst4, 'i'));//should print True
-        Console.WriteLine(IsExistsRecursive(lst4, 'I'));//should print False
+    Node<char> lst4 = new Node<char>('t', new Node<char>('A', new Node<char>('l', new Node<char>('s', new Node<char>('i')))));//['t', next]=>['a', next]=>['l', next]=>['s', next]=>['i', next]=>null
+    Console.WriteLine(IsExists(lst1, 5));//should print True
+    Console.WriteLine(IsExists(lst4, 'i'));//should print True
+    Console.WriteLine(IsExists(lst4, 'I'));//should print False
+    Console.WriteLine(IsExistsRecursive(lst1, 5));//should print True
+    Console.WriteLine(IsExistsRecursive(lst4, 'i'));//should print True
+    Console.WriteLine(IsExistsRecursive(lst4, 'I'));//should print False
 
 
-    }
+}
 
 
 }
